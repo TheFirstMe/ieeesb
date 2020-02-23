68a7a5a3852fb56bc4d410d3ae080a98f0ff75bc
Sourabh Subhod <sourabhsubhod@gmail.com>
2020-02-24 01:29:41 +0530

    change

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,51 @@
 /** @jsx jsx */
 import { jsx, Flex } from 'theme-ui'
 import Logo from "../../assets/svg/logo.svg";
+import { mediaQueries } from "../../design-tokens/media-queries"
+// import React from "react"
+// import { Link } from "gatsby"
+
+// const navItemHorizontalSpacing = [1, null, 2]
+
+// const navItemStyles = {
+//   borderBottom: `2px solid transparent`,
+//   color: `blue`,
+//   display: `block`,
+//   fontSize: 3,
+//   lineHeight: 1.5,
+//   // [mediaQueries.md]: {
+//   //   lineHeight: t => `calc(${t.sizes.headerHeight} - ${navItemTopOffset})`,
+//   // },
+//   position: `relative`,
+//   textDecoration: `none`,
+//   zIndex: 1,
+//   "&:hover, &:focus": { color: `navigation.linkHover` },
+// }
+
+// const NavItem = ({ to, children }) => (
+//   <li
+//     sx={{
+//       display: `block`,
+//       m: 0,
+//       mx: navItemHorizontalSpacing,
+//     }}
+//   >
+//     <Link
+//       to={to}
+//       activeClassName="active"
+//       partiallyActive={true}
+//       sx={{
+//         ...navItemStyles,
+//         "&.active": {
+//           borderBottomColor: `lilac`,
+//           color: `black`,
+//         },
+//       }}
+//     >
+//       {children}
+//     </Link>
+//   </li>
+// )
 
 const Navbar = (props) => (
   <header
@@ -33,15 +78,28 @@ const Navbar = (props) => (
         // },
       }}
     >
-      <Logo 
+      <Logo
         sx={{
           height: 40,
           fill: `#0b5172`,
         }}
       />
-      <div>
-        Test
-      </div>
+      {/* <nav
+        sx={{
+          display: `none`,
+          [mediaQueries.md]: {
+            alignSelf: `flex-end`,
+            display: `flex`,
+            listStyle: `none`,
+            m: 0,
+            maskImage: t =>
+              `linear-gradient(to right, transparent, white ${t.space[1]}, white 98%, transparent)`,
+            overflowX: `auto`,
+          },
+        }}
+      >
+        <NavItem to="/">Home</NavItem>
+      </nav> */}
     </Flex>
   </header>
 )
