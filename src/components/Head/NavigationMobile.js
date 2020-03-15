b47b32d4ab19597e41a40ca986691759722419c5
TheFirstMe <sourabhsubhod@gmail.com>
2020-03-16 00:40:54 +0530

    minor changes to navigation mobile

--- src/components/Head/NavigationMobile.js
+++ src/components/Head/NavigationMobile.js
@@ -3,34 +3,77 @@ import { jsx } from 'theme-ui'
 import React, { useState } from 'react';
 import Burger from '../burger';
 import { mediaQueries } from "../../design-tokens/media-queries"
+import { Link } from "gatsby"
 
 import { bool } from 'prop-types';
 
+
+const navItemStyles = {
+    borderBottom: `2px solid transparent`,
+    color: `navigation.linkDefault`,
+    display: `block`,
+    fontSize: 4,
+    lineHeight: 1.5,
+    textDecoration: `none`,
+    textAlign: `center`,
+    // zIndex: 1,
+    "&.active": { color: `navigation.linkActive`, }
+}
+
+const NavItem = ({ to, children }) => {
+    return (
+        <li
+            sx={{
+                display: `block`,
+                m: 0,
+                // mx: navItemHorizontalSpacing,
+                // px: navItemHorizontalSpacing
+            }}
+        >
+            <Link
+                to={to}
+                activeClassName="active"
+                partiallyActive={true}
+                sx={{
+                    ...navItemStyles,
+                    py: `13px`,
+                    // px: `30px`,
+                }}
+            >
+                {children}
+            </Link>
+
+        </li>
+    )
+}
+
 const menuStyles = {
+    fontFamily: `heading`,
     display: `flex`,
     flexDirection: `column`,
     justifyContent: `center`,
+    // alignSelf: `flex-end`,
     backgroundColor: `navigation.background`,
     height: `100vh`,
     textAlign: `left`,
-    padding: `2rem`,
+    py: 12,
     position: `absolute`,
     top: 0,
     left: 0,
     transition: `transform 0.2s ease-in-out`,
     width: `100%`,
-    a: {
-        fontSize: `1.5rem`,
-        textAlign: `center`,
-        textTransform: `uppercase`,
-        padding: `2rem 0`,
-        color: `pink`,
-        textDecoration: `none`,
-        transition: `color 0.3s linear`,
-        "&:hover": {
-            color: `blue`,
-        },
-    },
+    // a: {
+    //     fontSize: `1.5rem`,
+    //     textAlign: `center`,
+    //     textTransform: `uppercase`,
+    //     padding: `2rem 0`,
+    //     color: `pink`,
+    //     textDecoration: `none`,
+    //     transition: `color 0.3s linear`,
+    //     "&:hover": {
+    //         color: `blue`,
+    //     },
+    // },
 }
 
 const Menu = ({ open }) => {
@@ -39,7 +82,7 @@ const Menu = ({ open }) => {
             ...menuStyles,
             transform: open ? `translateX(0)` : `translateX(-100%)`,
         }}>
-            <a href="/">
+            {/* <a href="/">
                 <span role="img" aria-label="about us">&#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;</span>
         About us
       </a>
@@ -51,6 +94,12 @@ const Menu = ({ open }) => {
                 <span role="img" aria-label="contact">&#x1f4e9;</span>
         Contact
         </a>
+            <a href="/">
+                <span role="img" aria-label="contact">&#x1f4e9;</span>
+        Contact
+        </a> */}
+        <NavItem to={`/`}>Home</NavItem>
+        <NavItem to={`/`}>Home</NavItem>
         </nav>
     )
 }
